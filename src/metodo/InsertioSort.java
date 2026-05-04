9536b23358f5e4841413e00d347197859fd649d5
Andres <y2@Andress-MacBook-Pro.local>
2026-05-04 14:51:33 -0500

    last

--- src/metodo/InsertioSort.java
+++ src/metodo/InsertioSort.java
@@ -1,4 +1,7 @@
 package metodo;
+
+import java.util.Arrays;
+
 public class InsertioSort {
     public void metodoInsertionSort(int[] numeros,boolean asc) {
         int [] numeros1 = numeros.clone();
@@ -6,157 +9,139 @@ public class InsertioSort {
             int comparaciones = 0;
             int cambios = 0;
             int iteraciones = 0;
-
             System.out.println("==== METODO INSERCIÓN ====");
-            System.out.println("\nArreglo original:");
-            mostrarArreglo(numeros1);
+            System.out.println();
+            System.out.print("Arreglo original: ");
+            System.out.println(Arrays.toString(numeros1));
             System.out.println();
 
             for (int i = 1; i < numeros1.length; i++) {
+                int suma=0;
                 int clave = numeros1[i];
                 int j = i - 1;
 
-                System.out.printf("I%-2d  ", i);
-                mostrarArreglo(numeros1);
-
+                System.out.print("I:" + i + "  ");
+                System.out.println(Arrays.toString(numeros1));
                 while (j >= 0) {
                     comparaciones++;
-                    int ancho = numeros1.length * 5;
-                    StringBuilder margen = new StringBuilder();
-                    for (int k = 0; k < ancho; k++) margen.append(" ");
-
-                    System.out.printf("      %s a=%-1d  b=%-1d  [a]=%-4d [b]=%-4d  ", 
-                                    margen.toString(), j, j + 1, numeros1[j], clave);
+                    int a = j;
+                    int b = j + 1;
 
                     if (numeros1[j] > clave) {
-                        System.out.println("cambio=si");
-                        
-                        // --- AQUÍ SE FORMAN LAS 2 ESCALERAS ---
-                        System.out.print("      "); 
-                        for (int k = 0; k < numeros1.length; k++) {
-                            if (k == j) {
-                                System.out.printf("%-5s", "");
-                            } else if (k == j + 1) {
-                                System.out.printf("%-5d", numeros1[j]); 
-                            } else {
-                                System.out.printf("%-5s", "");
-                            }
-                        }
-                        System.out.print("\n      ");
-                        for (int k = 0; k < numeros1.length; k++) {
-                            if (k == j) System.out.printf("%-5d", clave);
-                            else System.out.printf("%-5s", "");
-                        }
-                        System.out.println();
-
                         numeros1[j + 1] = numeros1[j];
                         cambios++;
                         j--;
+                        
+                        System.out.println();
+                        System.out.print("     ");
+                        System.out.print(Arrays.toString(numeros1));
+                        System.out.print("     ");
+                        System.out.print("a=" + a + "  b=" + b + "  [a]=" + numeros1[a] + "  [b]=" + clave);
+                        System.out.println("  cambio=si");
+                        suma++;
                     } else {
-                        System.out.println("cambio=no");
+                        System.out.println();
+                        System.out.print("      ");
+                        System.out.print(Arrays.toString(numeros1));
+                        System.out.print("    cambio=no");
+                        System.out.println();
+
                         break;
                     }
                 }
                 numeros1[j + 1] = clave;
-
-                System.out.print("      ");
-                for (int k = 0; k < numeros1.length; k++) {
-                    if (k == j + 1) System.out.printf("%-5d", clave);
-                    else System.out.printf("%-5s", "");
+                if(suma==0){
+                     System.out.println();
+                }else{
+                    System.out.println();
+                    System.out.println("     " + "Arreglo tras reingresar el dato reservado");
+                    System.out.println();
+                    System.out.print("     ");
+                    System.out.println(Arrays.toString(numeros1));
+                    System.out.println();
                 }
-                System.out.println("\n");
                 iteraciones++;
             }
+            System.out.println();
+            System.out.println("======================================================");
+            System.out.println();
+            System.out.print("end:  ");
+           System.out.println(Arrays.toString(numeros1));
 
-            // AQUI SE MOSTRARA TODAS LAS COMPARACIONES,ITERACIONES Y CAMBIOS HECHA
-            System.out.print("end   ");
-            mostrarArreglo(numeros1);
-            System.out.println("\nCOMPARACIONES = " + comparaciones);
-            System.out.println("ITERACIONES   = " + iteraciones);
-            System.out.println("CAMBIOS       = " + cambios);
+            System.out.println();
+            
+            System.out.println("COMPARACIONES = " + comparaciones);
+            System.out.println("ITERACIONES = " + iteraciones);
+            System.out.println("CAMBIOS = " + cambios);
+            System.out.println();
         }else{
             int comparaciones = 0;
             int cambios = 0;
             int iteraciones = 0;
-
             System.out.println("==== METODO INSERCIÓN ====");
-            System.out.println("\nArreglo original:");
-            mostrarArreglo(numeros1);
+            System.out.println();
+            System.out.print("Arreglo original: ");
+            System.out.println(Arrays.toString(numeros1));
             System.out.println();
 
             for (int i = 1; i < numeros1.length; i++) {
+                int suma=0;
                 int clave = numeros1[i];
                 int j = i - 1;
 
-                System.out.printf("I%-2d  ", i);
-                mostrarArreglo(numeros1);
-
+                System.out.print("I:" + i + "  ");
+                System.out.println(Arrays.toString(numeros1));
                 while (j >= 0) {
                     comparaciones++;
-                    int ancho = numeros1.length * 5;
-                    StringBuilder margen = new StringBuilder();// Clase que nos permite manipular secuencias de caracteres. Siendo ideal para casos como estos debido a su poco consumo en memoria
-                    // a esta clase se le pueden añadir insert, appends o incluso delete para eliminar caracteres.
-                    // este StringBuilder no ahorra el hecho de crear multiples objetos de la clase String en memoria para posterior a ello concatenarlos
-                    for (int k = 0; k < ancho; k++) margen.append(" ");
-
-                    System.out.printf("      %s a=%-1d  b=%-1d  [a]=%-4d [b]=%-4d  ", 
-                                    margen.toString(), j, j + 1, numeros1[j], clave);
-
+                    int a = j;
+                    int b = j + 1;
                     if (numeros1[j] < clave) {
-                        System.out.println("cambio=si");
-                        
-                        // --- AQUÍ SE FORMAN LAS 2 ESCALERAS ---
-                        System.out.print("      "); 
-                        for (int k = 0; k < numeros1.length; k++) {
-                            if (k == j) {
-                                System.out.printf("%-5s", "");
-                            } else if (k == j + 1) {
-                                System.out.printf("%-5d", numeros1[j]); 
-                            } else {
-                                System.out.printf("%-5s", "");
-                            }
-                        }
-                        System.out.print("\n      ");
-                        for (int k = 0; k < numeros1.length; k++) {
-                            if (k == j) System.out.printf("%-5d", clave);
-                            else System.out.printf("%-5s", "");
-                        }
-                        System.out.println();
-
                         numeros1[j + 1] = numeros1[j];
                         cambios++;
                         j--;
+                        
+                        System.out.println();
+                        System.out.print("     ");
+                        System.out.print(Arrays.toString(numeros1));
+                        System.out.print("     ");
+                        System.out.print("a=" + a + "  b=" + b + "  [a]=" + numeros1[a] + "  [b]=" + clave);
+                        System.out.println("  cambio=si");
+                        suma++;
                     } else {
-                        System.out.println("cambio=no");
+                        System.out.println();
+                        System.out.print("    ");
+                        System.out.print(Arrays.toString(numeros1));
+                        System.out.print("    cambio=no");
+                        System.out.println();
                         break;
                     }
                 }
                 numeros1[j + 1] = clave;
-
-                System.out.print("      ");
-                for (int k = 0; k < numeros1.length; k++) {
-                    if (k == j + 1) System.out.printf("%-5d", clave);
-                    else System.out.printf("%-5s", "");
+                if(suma==0){
+                     System.out.println();
+                }else{
+                    System.out.println();
+                    System.out.println("     " + "Arreglo tras reingresar el dato reservado");
+                    System.out.println();
+                    System.out.print("     ");
+                    System.out.println(Arrays.toString(numeros1));
+                    System.out.println();
                 }
-                System.out.println("\n");
                 iteraciones++;
             }
-            // AQUI SE MOSTRARA TODAS LAS COMPARACIONES,ITERACIONES Y CAMBIOS HECHA
-            System.out.print("end   ");
-            mostrarArreglo(numeros1);
-            System.out.println("\nCOMPARACIONES = " + comparaciones);
-            System.out.println("ITERACIONES   = " +  iteraciones);
-            System.out.println("CAMBIOS       = " + cambios);
-        }
+            System.out.println();
+            System.out.println("======================================================");
+            System.out.println();
+            System.out.print("end:  ");
+            System.out.println(Arrays.toString(numeros1));
 
-    }
-
-    public static void mostrarArreglo(int[] numeros) {
-            int [] numeros1 = numeros;
-            for (int recorrer : numeros1) {
-                System.out.printf("%-5d", recorrer); // "printF" o print formatted es un tipo de print que nos permite alienar columanas en este caso pero tambien puede formatear tipos de datos
-            }
+            System.out.println();
+            
+            System.out.println("COMPARACIONES = " + comparaciones);
+            System.out.println("ITERACIONES = " + iteraciones);
+            System.out.println("CAMBIOS = " + cambios);
             System.out.println();
         }
+    }
 
 }
--- src/metodo/ShellSort.java
+++ src/metodo/ShellSort.java
@@ -1,167 +1,149 @@
 package metodo;
+import java.util.Arrays;
 public class ShellSort {
     public void metodoShellSort(boolean asc, int []numeros){
         int [] numeros1 = numeros.clone();
         if(asc){
             int comparaciones = 0;
             int cambios = 0;
-            int tamanio = numeros1.length;
             int iteraciones = 0;
-
-            System.out.println("==== MÉTODO SHELL SORT ====");
-            System.out.println("\nArreglo original:");
-            mostrarArreglo(numeros1);
+            int n=numeros1.length;
+            System.out.println("==== METODO SHELLSORT ====");
             System.out.println();
-
-            // el gap comienza en n/2 y se reduce a la mitad en cada iteración
-            for (int gap = tamanio / 2; gap > 0; gap /= 2) {
-                System.out.println("--- GAP = " + gap + " ---");
-                
-                for (int i = gap; i < tamanio; i++) {
+            System.out.print("Arreglo original: ");
+            System.out.println(Arrays.toString(numeros1));
+            System.out.println();
+            for (int gap=n/2;gap>0;gap/=2){
+                for (int i = gap; i < numeros1.length; i++) {
+                    iteraciones++;
+                    int suma=0;
                     int clave = numeros1[i];
                     int j = i;
-
-                    System.out.printf("I%-2d  ", i);
-                    mostrarArreglo(numeros1);
-
+                    System.out.print("I:" + iteraciones + "  ");
+                    System.out.println(Arrays.toString(numeros1));
                     while (j >= gap) {
                         comparaciones++;
-                        int a = j - gap;
-                        int b = j;
+                        int a = j;
+                        int b = j - gap;
 
-                        int ancho = numeros1.length * 5;
-                        StringBuilder margen = new StringBuilder();
-                        for (int k = 0; k < ancho; k++) margen.append(" ");
-
-                        System.out.printf("      %s a=%-1d  b=%-1d  [a]=%-4d [b]=%-4d  ", 
-                        margen.toString(), a, b, numeros1[a], clave);
-
-                        // --- AQUÍ SE FORMAN LAS 2 ESCALERAS ---
-                        if (numeros1[a] > clave) {
-                            System.out.println("cambio=si");
+                        if (numeros1[j-gap] > clave) {
+                            numeros1[j] = numeros1[j-gap];
+                            cambios++;
+                            j-=gap;
                             
-                            System.out.print("      "); 
-                            for (int k = 0; k < tamanio; k++) {
-                                if (k == b) System.out.printf("%-5d", numeros1[a]); 
-                                else System.out.printf("%-5s", "");
-                            }
-                            
-                            System.out.print("\n      ");
-                            for (int k = 0; k < tamanio; k++) {
-                                if (k == a) System.out.printf("%-5d", clave);
-                                else System.out.printf("%-5s", "");
-                            }
+                            System.out.println();
+                            System.out.print("     ");
+                            System.out.print(Arrays.toString(numeros1));
+                            System.out.print("     ");
+                            System.out.print("gap= "+ gap + "  a=" + a + "  b=" + b + "  [a]=" + numeros1[a] + "  [b]=" + clave);
+                            System.out.println("  cambio=si");
+                            suma++;
+                        } else {
+                            System.out.println();
+                            System.out.print("      ");
+                            System.out.print(Arrays.toString(numeros1));
+                            System.out.print("    cambio=no");
                             System.out.println();
 
-                            numeros1[b] = numeros1[a];
-                            cambios++;
-                            j -= gap;
-                        } else {
-                            System.out.println("cambio=no");
                             break;
                         }
                     }
                     numeros1[j] = clave;
-                    
-                    System.out.print("      ");
-                    for (int k = 0; k < tamanio; k++) {
-                        if (k == j) System.out.printf("%-5d", clave);
-                        else System.out.printf("%-5s", "");
+                    if(suma==0){
+                        System.out.println();
+                    }else{
+                        System.out.println();
+                        System.out.println("     " + "Arreglo tras reingresar el dato reservado");
+                        System.out.println();
+                        System.out.print("     ");
+                        System.out.println(Arrays.toString(numeros1));
+                        System.out.println();
                     }
-                    System.out.println("\n");
-                    iteraciones++;
                 }
             }
-             // AQUI SE MOSTRARA TODAS LAS COMPARACIONES,ITERACIONES Y CAMBIOS HECHA
-            System.out.print("end   ");
-            mostrarArreglo(numeros1);
-            System.out.println("\nCOMPARACIONES = " + comparaciones);
-            System.out.println("ITERACIONES   = " + iteraciones);
-            System.out.println("CAMBIOS       = " + cambios);
+            System.out.println();
+            System.out.println("======================================================");
+            System.out.println();
+            System.out.print("end:  ");
+           System.out.println(Arrays.toString(numeros1));
+
+            System.out.println();
+            
+            System.out.println("COMPARACIONES = " + comparaciones);
+            System.out.println("ITERACIONES = " + iteraciones);
+            System.out.println("CAMBIOS = " + cambios);
+            System.out.println();
         }else{
             int comparaciones = 0;
             int cambios = 0;
-            int tamanio = numeros1.length;
             int iteraciones = 0;
-
-            System.out.println("==== MÉTODO SHELL SORT ====");
-            System.out.println("\nArreglo original:");
-            mostrarArreglo(numeros1);
+            int n=numeros1.length;
+            System.out.println("==== METODO SHELLSORT ====");
             System.out.println();
-
-            // el gap comienza en n/2 y se reduce a la mitad en cada iteración
-            for (int gap = tamanio / 2; gap > 0; gap /= 2) {
-                System.out.println("--- GAP = " + gap + " ---");
-                
-                for (int i = gap; i < tamanio; i++) {
+            System.out.print("Arreglo original: ");
+            System.out.println(Arrays.toString(numeros1));
+            System.out.println();
+            for (int gap=n/2;gap>0;gap/=2){
+                for (int i = gap; i < numeros1.length; i++) {
+                    iteraciones++;
+                    int suma=0;
                     int clave = numeros1[i];
                     int j = i;
-
-                    System.out.printf("I%-2d  ", i);
-                    mostrarArreglo(numeros1);
-
+                    System.out.print("I:" + iteraciones + "  ");
+                    System.out.println(Arrays.toString(numeros1));
                     while (j >= gap) {
                         comparaciones++;
-                        int a = j - gap;
-                        int b = j;
+                        int a = j;
+                        int b = j - gap;
 
-                        int ancho = numeros1.length * 5;
-                        StringBuilder margen = new StringBuilder();
-                        for (int k = 0; k < ancho; k++) margen.append(" ");
-
-                        System.out.printf("      %s a=%-1d  b=%-1d  [a]=%-4d [b]=%-4d  ", 
-                        margen.toString(), a, b, numeros1[a], clave);
-
-                        // --- AQUÍ SE FORMAN LAS 2 ESCALERAS ---
-                        if (numeros1[a] < clave) {
-                            System.out.println("cambio=si");
+                        if (numeros1[j-gap] < clave) {
+                            numeros1[j] = numeros1[j-gap];
+                            cambios++;
+                            j-=gap;
                             
-                            System.out.print("      "); 
-                            for (int k = 0; k < tamanio; k++) {
-                                if (k == b) System.out.printf("%-5d", numeros1[a]); 
-                                else System.out.printf("%-5s", "");
-                            }
-                            
-                            System.out.print("\n      ");
-                            for (int k = 0; k < tamanio; k++) {
-                                if (k == a) System.out.printf("%-5d", clave);
-                                else System.out.printf("%-5s", "");
-                            }
+                            System.out.println();
+                            System.out.print("     ");
+                            System.out.print(Arrays.toString(numeros1));
+                            System.out.print("     ");
+                            System.out.print("gap= "+ gap + "  a=" + a + "  b=" + b + "  [a]=" + numeros1[a] + "  [b]=" + clave);
+                            System.out.println("  cambio=si");
+                            suma++;
+                        } else {
+                            System.out.println();
+                            System.out.print("      ");
+                            System.out.print(Arrays.toString(numeros1));
+                            System.out.print("    cambio=no");
                             System.out.println();
 
-                            numeros1[b] = numeros1[a];
-                            cambios++;
-                            j -= gap;
-                        } else {
-                            System.out.println("cambio=no");
                             break;
                         }
                     }
                     numeros1[j] = clave;
-                    
-                    System.out.print("      ");
-                    for (int k = 0; k < tamanio; k++) {
-                        if (k == j) System.out.printf("%-5d", clave);
-                        else System.out.printf("%-5s", "");
+                    if(suma==0){
+                        System.out.println();
+                    }else{
+                        System.out.println();
+                        System.out.println("     " + "Arreglo tras reingresar el dato reservado");
+                        System.out.println();
+                        System.out.print("     ");
+                        System.out.println(Arrays.toString(numeros1));
+                        System.out.println();
                     }
-                    System.out.println("\n");
-                    iteraciones++;
                 }
             }
-             // AQUI SE MOSTRARA TODAS LAS COMPARACIONES,ITERACIONES Y CAMBIOS HECHA
-            System.out.print("end   ");
-            mostrarArreglo(numeros1);
-            System.out.println("\nCOMPARACIONES = " + comparaciones);
-            System.out.println("ITERACIONES   = " + iteraciones);
-            System.out.println("CAMBIOS       = " + cambios);
-        }
-    }
+            System.out.println();
+            System.out.println("======================================================");
+            System.out.println();
+            System.out.print("end:  ");
+           System.out.println(Arrays.toString(numeros1));
 
-    public static void mostrarArreglo(int[] numeros){
-        int [] numeros1 = numeros;
-        for (int recorrer : numeros1){
-            System.out.printf("%-5d", recorrer);
+            System.out.println();
+            
+            System.out.println("COMPARACIONES = " + comparaciones);
+            System.out.println("ITERACIONES = " + iteraciones);
+            System.out.println("CAMBIOS = " + cambios);
+            System.out.println();
         }
-        System.out.println();
     }
+    
 }
